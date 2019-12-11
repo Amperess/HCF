f314b47ca2ec4bdc435588908a758874da1c524c
None <None>
2019-12-11 14:52:50 -0800

    Added static routing step into script'

--- testrouter2.py
+++ testrouter2.py
@@ -1,26 +1,5 @@
 #!/usr/bin/python
 
-"""
-linuxrouter.py: Example network with Linux IP router
-This example converts a Node into a router using IP forwarding
-already built into Linux.
-The example topology creates a router and three IP subnets:
-    - 192.168.1.0/24 (r0-eth1, IP: 192.168.1.1)
-    - 172.16.0.0/12 (r0-eth2, IP: 172.16.0.1)
-    - 10.0.0.0/8 (r0-eth3, IP: 10.0.0.1)
-Each subnet consists of a single host connected to
-a single switch:
-    r0-eth1 - s1-eth1 - h1-eth0 (IP: 192.168.1.100)
-    r0-eth2 - s2-eth1 - h2-eth0 (IP: 172.16.0.100)
-    r0-eth3 - s3-eth1 - h3-eth0 (IP: 10.0.0.100)
-The example relies on default routing entries that are
-automatically created for each router interface, as well
-as 'defaultRoute' parameters for the host interfaces.
-Additional routes may be added to the router or hosts by
-executing 'ip route' or 'route' commands on the router or hosts.
-"""
-
-
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.node import Node
@@ -57,11 +36,19 @@ class NetworkTopo( Topo ):
                            defaultRoute='via 10.0.2.100' )
 	self.addLink( router1, h1, intfName1='r1-eth1', intfName2='h1-eth0', params1 = {'ip':'10.0.1.10/24'}, params2={'ip':'10.0.1.100/24'})
 	self.addLink( router2, h2, intfName1='r2-eth1', intfName2='h2-eth0', params1 = {'ip':'10.0.2.20/24'}, params2={'ip':'10.0.2.100/24'})
+
 def run():
     "Test linux router"
     topo = NetworkTopo()
     net = Mininet( topo=topo )  # controller is used by s1-s3
     net.start()
+
+    router1 = net.get('router1')
+    router2 = net.get('router2')
+
+    router1.cmd('ip route add 10.0.2.0/24 dev r1-eth0')    
+    router2.cmd('ip route add 10.0.1.0/24 dev r2-eth0')
+
     info( '*** Routing Table on Router:\n' )
     info( net[ 'router1' ].cmd( 'route' ) )
     CLI( net )
